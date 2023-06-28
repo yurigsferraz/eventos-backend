4cfc3a38810278d2c23b8977d9990164d2687f63
Yuri Ferraz <yurigsf@dcc.ufrj.br>
2023-06-28 03:51:13 -0300

    importa Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { TipoEventoModule } from './tipo-evento/tipo-evento.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [EventsModule, TipoEventoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
